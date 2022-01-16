33736132fa288bb1dc7c9bd12e4a561b2b03f5e9
Marvin Tunji-ola <marvinsmart9@gmail.com>
2022-01-16 23:00:36 +0100

    chore: secure environment

--- src/services/voiceflow.tsx
+++ src/services/voiceflow.tsx
@@ -1,9 +1,7 @@
 import { BaseRequest, GeneralTrace, RequestType } from '@voiceflow/general-types';
 import axios from 'axios';
 
-// const versionID = 1;
-// eslint-disable-next-line no-secrets/no-secrets
-const APIKey = 'VF.DM.61e37442233b3d001b9b469e.c65ELDiJSpkO8qSi';
+const APIKey = process.env.REACT_APP_VOICE_FLOW_API_KEY;
 
 const interact = async (userId: string, message: string, type: string = RequestType.TEXT): Promise<GeneralTrace[]> => {
   const request: BaseRequest = { type, payload: message };
