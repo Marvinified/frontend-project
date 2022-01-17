9bca62dcb9f7972559f226d3e48bfefa9ad5693b
Marvin Tunji-ola <marvinsmart9@gmail.com>
2022-01-17 11:23:07 +0100

    update title

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,7 +6,7 @@ import AddNewUser from 'src/containers/AddNewUser';
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
-      <h2>VoiceBot</h2>
+      <h2>Pizza Bot</h2>
       <AddNewUser />
     </nav>
   );
